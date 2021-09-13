c29d16377739d7f8fdfd4d3d83b4e250c967b1e9
Simão Gomes Viana <simao@halogenos.org>
2021-09-13 20:22:04 +0200

    Checkout DeviceSettings from XOS-11.0-20210913

--- DeviceSettings/src/com/derp/device/DeviceSettings/Startup.java
+++ DeviceSettings/src/com/derp/device/DeviceSettings/Startup.java
@@ -25,8 +25,6 @@ import android.provider.Settings;
 import android.text.TextUtils;
 import androidx.preference.PreferenceManager;
 
-import com.derp.device.DeviceSettings.TouchscreenGestureSettings;
-
 public class Startup extends BroadcastReceiver {
 
     private static final String TAG = "BootReceiver";
@@ -51,7 +49,6 @@ public class Startup extends BroadcastReceiver {
     @Override
     public void onReceive(final Context context, final Intent bootintent) {
         boolean enabled = false;
-        TouchscreenGestureSettings.MainSettingsFragment.restoreTouchscreenGestureStates(context);
         SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
         VibratorStrengthPreference.restore(context);
     }
