f5936a40dd2d16193474d118aec5881018982657
Simão Gomes Viana <simao@halogenos.org>
2021-09-13 18:04:46 +0200

    KJHDGnj adKOIJ4WET HUSDFGR IHBDFG O IJO

--- common.mk
+++ common.mk
@@ -8,7 +8,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
 # Get non-open-source specific aspects
-$(call inherit-product, vendor/oneplus/google/google.mk)
+#$(call inherit-product, vendor/oneplus/google/google.mk)
 $(call inherit-product, vendor/oneplus/sm8150-common/sm8150-common-vendor.mk)
 
 # Project ID Quota
