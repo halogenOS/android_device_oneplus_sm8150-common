9f15d5d25f206cd2260f183d003a9219190e93ed
Master <mhmdeveloperofficial@gmail.com>
2023-10-12 17:41:32 +0800

    Disable sparse for ext images
    * Sparse images cause kPostInstallMountError
    
    Co-authored-by: yairpatch <yair.zarfati@gmail.com>
    Change-Id: Ifdb2fc87d88c9c2139b3e73b6ccad36f09888b75
    Signed-off-by: ralph950412 <ralph950412@gmail.com>
    Signed-off-by: Simão Gomes Viana <devel@superboring.dev>
    Signed-off-by: Noah <noah.anleitner@halogenos.org>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -133,6 +133,9 @@ TARGET_MODULE_ALIASES += \
 # Lineage Health
 TARGET_HEALTH_CHARGING_CONTROL_CHARGING_PATH := /sys/class/power_supply/battery/mmi_charging_enable
 
+# Disable sparse for ext images
+TARGET_USERIMAGES_SPARSE_EXT_DISABLED := true
+
 # Platform
 BOARD_USES_QCOM_HARDWARE := true
 TARGET_BOARD_PLATFORM := msmnile
