fe5e788660ca6fb16ece849d0ef468b1a68842b1
Lup Gabriel <gwolf2u.website@gmail.com>
2024-02-22 20:37:13 +0200

    Revert "sm8150-common: enable blur"
    This reverts commit 43392cbc917113c1e876468966e04f0b77e80aff.
    * Causes frame drops in launcher
    
    Signed-off-by: Noah Anleitner <noah.anleitner@halogenos.org>

--- common.mk
+++ common.mk
@@ -495,9 +495,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     WfdCommon
 
-# Blur
-TARGET_ENABLE_BLUR := true
-
 # Thermal
 PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0-service.qti
