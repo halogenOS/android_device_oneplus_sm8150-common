e618aba5b7d3397e7f1e8e429a1a40a571e2d6bc
Noah Anleitner <noah.anleitner@halogenos.org>
2024-03-06 17:28:04 +0100

    Revert "sm8150-common: Switch to power-libperfmgr"
    This reverts commit 93182f6af6943558cb5a82ab9c143b25530048a2.

--- common.mk
+++ common.mk
@@ -342,11 +342,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libqti-perfd-client
 
+# Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.2.vendor
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+    android.hardware.power-service-qti
 
 # QMI
 PRODUCT_PACKAGES += \
