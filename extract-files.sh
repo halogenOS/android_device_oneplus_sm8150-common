6394a8917fd92b38e858cb0d1498c4d0717db9a2
SamarV-121 <samar@samarv121.dev>
2024-09-05 08:13:08 +0530

    sm8150-common: Link older libcrypto for widevine blobs
    https://github.com/google/boringssl/commit/e202e51cb0912f36dafbd2e67cf04d6ec82f3180
    
    Change-Id: Ic32d5103d42cd28ccbc8055e766288dfefc1c02d
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>
    Signed-off-by: Simão Gomes Viana <devel@superboring.dev>

--- extract-files.sh
+++ extract-files.sh
@@ -87,6 +87,9 @@ function blob_fixup() {
         vendor/lib64/hw/com.qti.chi.override.so)
             grep -q libcamera_metadata_shim.so "${2}" || "${PATCHELF}" --add-needed libcamera_metadata_shim.so "${2}"
             ;;
+        vendor/lib64/mediadrm/libwvdrmengine.so|vendor/lib64/libwvhidl.so )
+            grep -q "libcrypto-v33.so" "${2}" || "${PATCHELF}" --replace-needed "libcrypto.so" "libcrypto-v33.so" "${2}"
+            ;;
     esac
 }
 
