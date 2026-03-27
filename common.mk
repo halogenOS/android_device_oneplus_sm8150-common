b98deacfbef5b31caa39cc0afb72a7eb686031a4
Noah Anleitner <noah.anleitner@nonetwor.cc>
2026-03-27 22:02:28 +0100

    sm8150-common: Remove dead LineageOS SDK overlays

--- common.mk
+++ common.mk
@@ -228,9 +228,6 @@ PRODUCT_COPY_FILES += \
 $(call inherit-product, hardware/oplus/overlay/generic/generic.mk)
 $(call inherit-product, hardware/oplus/overlay/qssi/qssi.mk)
 
-DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage
-
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_PACKAGES += \
     CarrierConfigResCommon \
